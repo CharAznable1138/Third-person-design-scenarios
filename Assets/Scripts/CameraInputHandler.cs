fbd7b8e419fc6fc13aacd0a36ca96a3333598ab4
Max Garay <57268377+CharAznable1138@users.noreply.github.com>
2020-10-18 21:34:43 -0500

    created an input handler to use the new input system for cinemachine

---
+++ Assets/Scripts/CameraInputHandler.cs
@@ -0,0 +1,20 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraInputHandler : MonoBehaviour
+{
+    private CinemachineFreeLook cinemachineFreeLook;
+    private void Start()
+    {
+        cinemachineFreeLook = GetComponent<CinemachineFreeLook>();
+    }
+    public void OnLook(InputAction.CallbackContext _context)
+    {
+        var pointerDelta = _context.ReadValue<Vector2>();
+        cinemachineFreeLook.m_XAxis.Value += pointerDelta.x;
+        cinemachineFreeLook.m_YAxis.Value += pointerDelta.y;
+    }
+}
